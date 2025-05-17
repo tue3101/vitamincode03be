00acd2f6f1a7b5cd6e612eb16bf5c3e4d3bd9315
TueNguyen <keynguyen3101@gmail.com>
2025-05-17 16:10:40 +0700

    Commit02: Add new print in Main.java line 6

--- src/main/com/vitamincode03/main.java
+++ src/main/com/vitamincode03/main.java
@@ -3,5 +3,6 @@ package main.com.vitamincode03;
 public class main {
     public static void main(String[] args) {
         System.out.print("hello vitamin code 03 - git! - TueNguyen");
+        System.out.println("hello admin");
     }
 }
