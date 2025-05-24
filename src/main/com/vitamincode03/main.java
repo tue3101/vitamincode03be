6e22b706195b0317a1fc3673695722068ac4d23f
QuocDat932 <datbui185@gmail.com>
2025-05-24 14:49:08 +0700

    Commit03: Add new calNumberANumberB() - update gitignore to remove file misc

--- src/main/com/vitamincode03/main.java
+++ src/main/com/vitamincode03/main.java
@@ -1,8 +1,30 @@
 package main.com.vitamincode03;
 
+import java.util.Scanner;
+
 public class main {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.print("hello vitamin code 03 - git! - TueNguyen");
         System.out.println("hello admin");
+        calNumberANumberB();
+    }
+
+    public static void calNumberANumberB() {
+        System.out.println("Nhap vao hai so : ");
+        System.out.print("a = ");
+        int a = sc.nextInt();
+        System.out.print("b = ");
+        int b = sc.nextInt();
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        if (b != 0) {
+            System.out.println("a / b = " + (float)(a / b));
+        } else {
+            System.out.println("Không thể chia cho 0");
+        }
     }
 }
