0ce97615ce1824ee842cb12e590d658b91da106d
TueNguyen <keynguyen3101@gmail.com>
2025-05-24 15:06:49 +0700

    Commit04: new_function_tinhnghiem

--- src/main/com/vitamincode03/main.java
+++ src/main/com/vitamincode03/main.java
@@ -1,8 +1,33 @@
 package main.com.vitamincode03;
 
+import java.util.Scanner;
+
 public class main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.print("hello vitamin code 03 - git! - TueNguyen");
         System.out.println("hello admin");
+
+        timnghiem();
     }
+    public static void timnghiem() {
+        System.out.println("Nhap vao cac he so a, b, c : ");
+        System.out.print("a = ");
+        int a = sc.nextInt();
+        System.out.print("b = ");
+        int b = sc.nextInt();
+        System.out.print("c = ");
+        int c = sc.nextInt();
+        if (b != 0) {
+            float x = (float) (c - a) / b;
+            System.out.println("nghiem x = " + x);
+        } else {
+            if (a == c) {
+                System.out.println("Phuong trinh vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        }
+   }
+
 }
